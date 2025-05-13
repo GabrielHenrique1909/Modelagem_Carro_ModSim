57bdc2953a011c8eb030d4bb81365fd95d21b2fd
CarlosNeto242 <carloslmn@al.insper.edu.br>
2025-05-13 10:45:33 -0300

    Declarando algumas constantes

---
+++ constantes.py
@@ -0,0 +1,6 @@
+Cw = 0.373 # coeficiente aerodinâmico
+A = 2.04 # área frontal do carro (m²)
+p = 1.125 # densidade do ar (Kg/m³)
+m = 1250 # massa do carro parcialmente preenchido (Kg)
+r = 0.436 # raio da roda (m)
+n = {"1" : 4.667, "2" : 2.533, "3" : 1.556, "4" : 1.135, "5" : 0.859} # relação de transmissão para diferentes marchas
